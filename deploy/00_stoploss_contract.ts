e7019f0890690604b1c4df97d3ec68bc0fb905d4
Harry Trippel <harrytrippel@gmx.de>
2021-10-26 11:13:18 +0200

    save changes

--- deploy/00_stoploss_contract.ts
+++ deploy/00_stoploss_contract.ts
@@ -21,51 +21,12 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     args: [deploymentProps.factory, deploymentProps.WETH, deploymentProps.factroy_code_hash],
     log: true,
   });
-//   const GelatoPineCore = await deploy('RelayerPineCore', {
-//     from: deployer,
-//     args: [deployer],
-//     log: true,
-//   });
+
  
   
 };
 
-// const encodedData = this._handlerAddress
-//  this._abiEncoder.encode(
-//     ["address", "uint256", "address"],
-//     [outputToken, minReturn, this._handlerAddress]
 
-
-// const encodedEthOrder = await this._gelatoLimitOrders.encodeEthOrder(
-//     this._moduleAddress,
-//     ETH_ADDRESS, // we also use ETH_ADDRESS if it's MATIC
-//     owner,
-//     witness,
-//     encodedData,
-//     secret
-//   );
-//   data = this._gelatoLimitOrders.interface.encodeFunctionData(
-//     "depositEth",
-//     [encodedEthOrder]
-//   );
-//   value = amount;
-//   to = this._gelatoLimitOrders.address;
-
-//   data = this._erc20OrderRouter.interface.encodeFunctionData(
-//     "depositToken",
-//     [
-//       amount,
-//       this._moduleAddress,
-//       inputToken,
-//       owner,
-//       witness,
-//       encodedData,
-//       secret,
-//     ]
-//   );
-//   value = constants.Zero;
-//   to = this._erc20OrderRouter.address;
-  
 
 
 export default func;
